f362284e5c7e546ce4fbac0024935261fed796cb
Jouse <josuecanalesmena@gmail.com>
2019-03-06 19:47:32 -0600

    avance: GridLayout dinánico
    se puede parametrizar columnas y filas.
    TODO:
    -proporcion de celdas
    -se cae ocasionalmente

--- LabTetris/app/src/main/java/com/canales/josue/curso/tetris/MainActivity.java
+++ LabTetris/app/src/main/java/com/canales/josue/curso/tetris/MainActivity.java
@@ -2,8 +2,9 @@ package com.canales.josue.curso.tetris;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayout;
+import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.GridLayout;
 import android.widget.ImageView;
 
 import com.canales.josue.curso.tetris.Classes.TetrisAdapter;
@@ -15,12 +16,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        generateTetris(20,20);
 
     }
 
-    private void generateTetris(int rowCount, int columnCount){
-        GridLayout tetrisGridLayout = findViewById(R.id.TetrisGridLayout);
+    private void generateTetris( int rowCount, int columnCount){
 
+        GridLayout tetrisGridLayout = (GridLayout) findViewById(R.id.mytetrisboard);
 
         //adapter del tetris TetrisAdapter adapter = new TetrisAdapter(this,new int[] {R.drawable.gray,})
 
@@ -28,13 +30,21 @@ public class MainActivity extends AppCompatActivity {
         tetrisGridLayout.setColumnCount(columnCount);
         tetrisGridLayout.setRowCount(rowCount);
         for (int i = 0; i < rowCount; i++){
-            for (int j = 0; j < columnCount; columnCount++ ){
-                ImageView child = (ImageView) tetrisGridLayout.getChildAt(i*rowCount + columnCount);
-                child.setImageResource(R.drawable.gray);
+            for (int j = 0; j < columnCount; j++ ){
+                ImageView child = new ImageView(this);
+                child.setLayoutParams(new ViewGroup.LayoutParams(42,42));
+                if (i == 0 || j == 0 || i + 1 == rowCount || j+1 == columnCount ){
+                    child.setImageResource(R.drawable.gray);
+                }else {
+                    child.setImageResource(R.drawable.black);
+                }
                 child.setMaxWidth((int) (tetrisGridLayout.getWidth()/columnCount));
                 child.setMaxHeight((int) (tetrisGridLayout.getHeight()/rowCount));
+                tetrisGridLayout.addView(child, i*rowCount + j);
             }
         }
     }
+
+
 
 }
